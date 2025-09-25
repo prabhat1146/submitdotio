52102928c552c1385d6a0aa0744cdfb14860b146
prabhat kumar <prabhat9468@gmail.com>
2025-09-25 16:37:42 +0530

    test commit

--- src/pages/components/auth/Login.jsx
+++ src/pages/components/auth/Login.jsx
@@ -42,7 +42,7 @@ const Login = () => {
         })
         .catch((err) => {
           console.log(err);
-          alert(err.response.error || "Server error");
+          alert(err?.response?.error || "Server error");
         })
         .finally((final) => {
           setLoading(false);
